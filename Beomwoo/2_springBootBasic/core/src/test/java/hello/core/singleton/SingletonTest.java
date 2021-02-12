3053a597f75fafd3b65b79bfb31f9e81aa3b9c61
Beomwoo <doorbw@outlook.com>
2021-02-13 01:21:11 +0900

    singletone precautions

--- Beomwoo/2_springBootBasic/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ Beomwoo/2_springBootBasic/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
@@ -39,5 +41,20 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         //same : ==
         //equal : equals
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
